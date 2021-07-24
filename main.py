5b56730fd18aea5b0b3f337a44a4233a494ac636
divadiahim <marian1974@gmail.com>
2021-07-25 00:22:27 +0300

    v1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils.array_init import init_random_array, reset_array
 from utils.button import Button
 from pygame import Color, init
 from utils import *
-
+import time
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Sorting algorithm visualiser")
 img_run = pygame.image.load("img/play.png").convert_alpha()
@@ -15,6 +15,7 @@ def init_grid(rows,cols,color):
         grid.append([])
         for j in range(cols):
             grid[i].append(color)
+            
 
     return grid       
 
@@ -31,33 +32,50 @@ def draw_grid(win,grid):
         for j in range(COLS+1):
             pygame.draw.line(win,BLACK,(j*PIXEL_SIZE,0),(j*PIXEL_SIZE,HEIGHT-TOOLBAR_HEIGHT))
 
-
+c=0
+global ok
+ok=0
 def draw(win,grid):
     win.fill(BG_COLOR)
     draw_grid(win,grid)
-    if(create_button.draw(WIN)):
-        init_random_array(grid)
+    pixels=[]
+    
+                  
     run_button.draw(WIN)
     if(reset_button.draw(WIN)):
-        reset_array(grid)  
-    pygame.display.update()
+        reset_array(grid) 
+       
+   
+   
 
 running = True #create the running var
 clock = pygame.time.Clock() #create the game clock so that it`s running at capped fps
 grid = init_grid(ROWS,COLS,WHITE)#create the grid of pixels to be filled 
-
+random_grid=init_random_array(grid)
 
 # create button instances #
 run_button=button.Button_img(img_run,10,615,0.15,None)
 create_button=button.Button('create',200,40,(390,630))
 reset_button=button.Button('reset',200,40,(170,630))
-
+pixels=[(0,0)]
 while running:
-    clock.tick(FPS)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
     draw(WIN,grid)
-    # grid[0][6]=BLACK
+    if(create_button.draw(WIN)):
+        randomlist=random_grid.init_random_array()
+        pixels=random_grid.fiil_grid()
+        c=1
+    if(c==0):
+        pass
+    elif(c<len(pixels)-1):
+        print(c)
+        c=c+1    
+        x,y=pixels[c]
+        grid[x][y]=BLACK
+    pygame.display.update()    
     
 pygame.quit()            
--- utils/array_init.py
+++ utils/array_init.py
@@ -1,19 +1,41 @@
 from .settings import *
 import random
+import time
 
-def init_random_array(grid):
-    randomlist = []
-    for i in range (0,100):
-        n = random.randint(1,100)
-        randomlist.append(n)
-    print(randomlist)
-    for z in range(len(randomlist)):
-        curr=randomlist[z]
-        # print(curr)
-        print(z)
-        for j in range(curr):
-            grid[j][z]=BLACK
-
+class init_random_array:
+    def __init__(self,grid):
+        self.grid=grid
+        
+    def init_random_array(self):
+        self.randomlist = []
+        for i in range (0,LIST_LEN):
+            n = random.randint(1,100)
+            self.randomlist.append(n)
+        self.randomlist_len=len(self.randomlist)
+        return self.randomlist 
+    def fiil_grid(self):
+        pixels=[]       
+        for z in range(self.randomlist_len):
+            pygame.time.delay(2)
+            curr=self.randomlist[z]
+            # print(curr)
+            # print(z)
+            for j in range(curr):
+                pixels.append(tuple((j,z)))
+                # self.grid[j][z]=BLACK
+            pygame.display.update()        
+        return pixels           
+    # def fiil_grid_test(self,j,z):
+    #     self.grid[j].append((BLACK))
+    #     return  
+    # def fiil_grid_recc(self):
+    #     if LIST_LEN<self.z:
+    #         return 0 
+    #     self.grid[]       
+    #     self.fill_grid_recc(self.randomlist[self.z+1])    
+                
+            
+              
 def reset_array(grid):
     for i in range(ROWS):
         for j in range(COLS):
